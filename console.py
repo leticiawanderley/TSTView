cc96c486904b03ff7c164c616d0c42425d7573cf
Matheus Gaudencio <matheusgr@gmail.com>
2014-04-05 04:32:18 -0300

    Removed view attrs from Console entity.

--- console.py
+++ console.py
@@ -1,11 +1,9 @@
 #  -*- coding: utf-8 -*-
 
 # import logging
-import cgi
 from google.appengine.api import mail
 from google.appengine.api import users
 import webapp2
-import urllib2
 from datetime import datetime, timedelta
 from logica_console import Console
 import model
@@ -15,6 +13,71 @@ import jinja2
 DEBUG = False
 
 jinja_environment = jinja2.Environment(extensions=['jinja2.ext.i18n', 'jinja2.ext.loopcontrols'], loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+
+
+class Fields(dict):
+
+    def __new_setitem__(self, value):
+        raise Exception("frozen dict")
+
+    def __init__(self, request):
+        self['datainicio'] = request.get("di", default_value="")
+        self['datafim'] = request.get("df", default_value="")
+        self['horainicio'] = request.get("hi", default_value="")
+        self['horafim'] = request.get("hf", default_value="")
+        self['tabela_grafico'] = request.get("tg", default_value="")
+        self['turma'] = request.get("trm", default_value="")
+        self['questoes'] = request.get("qts", default_value="")
+        self.__setitem__ = self.__new_setitem__
+
+
+def trata_parametros(datainicio, horainicio, datafim, horafim, questoes):
+
+    hoje = datetime.now() - timedelta(0, 3 * 3600)
+    if not horainicio:
+        if 8 <= hoje.hour <= 17:
+            horainicial = timedelta(
+                0, (hoje.hour - (hoje.hour % 2)) * 3600)
+        else:
+            horainicial = timedelta(0, 0)
+    else:
+        hora = "%02d" % int(horainicio[0:2])
+        minutos = "%02d" % int(horainicio[2:4])
+        horainicial = timedelta(
+            0, (int(hora) * 3600) + (int(minutos) * 60))
+
+    """ Tratamento Parametro Hora Fim """
+    if not horafim:
+        horafinal = timedelta(
+            0, (hoje.hour * 3600) + (hoje.minute * 60) + hoje.second)
+    else:
+        hora = horafim[0:2]
+        minutos = horafim[2:4]
+        horafinal = timedelta(0, (int(hora) * 3600) + (int(minutos) * 60))
+
+    """ Tratamento Parametro Data Fim """
+    if not datafim:
+        datafinal = datetime(hoje.year, hoje.month, hoje.day) + horafinal
+    else:
+        datafinal = datetime.strptime(datafim, "%d%m%Y") + horafinal
+
+    """ Tratamento Parametro Data Inicio """
+    if not datainicio:
+        datainicial = datetime(
+            hoje.year, hoje.month, hoje.day) + horainicial
+    else:
+        if horafim and not datafim:
+            datainicial = datetime.strptime(datainicio, "%d%m%Y") + horainicial
+            datafinal = datetime.strptime(datainicio, "%d%m%Y") + horafinal
+        else:
+            datainicial = datetime.strptime(datainicio, "%d%m%Y") + horainicial
+
+    if questoes:
+        questoes = questoes.replace(" ", "").split(",")
+    else:
+        questoes = []
+
+    return datainicial, datafinal, questoes
 
 
 def checa_erros(datainicio, datafim, horainicio, horafim, tabela_grafico, turma):
@@ -75,16 +138,16 @@ def checa_erros(datainicio, datafim, horainicio, horafim, tabela_grafico, turma)
 
 class WebApp(webapp2.RequestHandler):
 
-    def cabecalho(self, console):
+    def cabecalho(self, datainicial, datafinal):
         cabecalho_values = {
-            "print_intervalo": u"Submissões no intervalo (Das %s de %s às %s):" % (datetime.strftime(console.datainicio, "%H:%M"),
-                                                                                     datetime.strftime(console.datainicio, "%d/%m/%Y"),
-                                                                                     datetime.strftime(console.datafim, "%H:%M de %d/%m/%Y"))}
+            "print_intervalo": u"Submissões no intervalo (Das %s de %s às %s):" % (datetime.strftime(datainicial, "%H:%M"),
+                                                                                     datetime.strftime(datainicial, "%d/%m/%Y"),
+                                                                                     datetime.strftime(datafinal, "%H:%M de %d/%m/%Y"))}
 
         jinja_env = jinja_environment.get_template("templates/cabecalho.html")
         self.response.out.write(jinja_env.render(cabecalho_values))
 
-    def grafico(self, console):
+    def grafico(self, console, datainicial, datafinal):
 
         sumario = console.sumario_grafico()
         grafico_values = {
@@ -95,27 +158,17 @@ class WebApp(webapp2.RequestHandler):
             "fizeram_certo": sumario[1],
             "fizeram_errado": sumario[3],
             "nao_fizeram": sumario[5],
-            "data_report": datetime.strftime(console.datainicio, "%d%m%Y"),
-            "dataf_report": datetime.strftime(console.datafim, "%d%m%Y")
+            "data_report": datetime.strftime(datainicial, "%d%m%Y"),
+            "dataf_report": datetime.strftime(datafinal, "%d%m%Y")
         }
         grafico_env = jinja_environment.get_template(
             "templates/grafico.html")
 
         self.response.out.write(grafico_env.render(grafico_values))
 
-    def ajax_replace(self, console, tabela_grafico):
-
-        ajax_values = {
-            "turma": console.lista_matriculas,
-            "questoes": ",".join(console.lista_questoes),
-            "datainicio": datetime.strftime(console.datainicio, "%d%m%Y"),
-            "horainicio": console.horainicio,
-            "datafim": datetime.strftime(console.datafim, "%d%m%Y"),
-            "horafim": console.horafim,
-            "tabela_grafico": tabela_grafico
-        }
+    def ajax_replace(self, fields, console):
+        ajax_values = dict(fields)
         ajax = jinja_environment.get_template("templates/ajax.html")
-
         self.response.out.write(ajax.render(ajax_values))
 
     def get(self):
@@ -123,102 +176,54 @@ class WebApp(webapp2.RequestHandler):
         self.response.headers["Content-Type"] = "text/html; charset=utf-8"
 
         user, is_adm, email = model.info(users.get_current_user())
-        datainicio = self.request.get("di", default_value="")
-        datafim = self.request.get("df", default_value="")
-        horainicio = self.request.get("hi", default_value="")
-        horafim = self.request.get("hf", default_value="")
-        tabela_grafico = self.request.get("tg", default_value="")
-        turma = self.request.get("trm", default_value="")
-        questoes = self.request.get("qts", default_value="")
 
-        if user:
-            self.response.out.write(self.login(
-                True, datainicio, datafim, horainicio, horafim, tabela_grafico, turma, questoes))
-        else:
-            self.response.out.write(self.login(
-                False, datainicio, datafim, horainicio, horafim, tabela_grafico, turma, questoes))
+        fields = Fields(self.request)
 
-        lista_turma, lista_questoes = self.trata_parametros(turma, questoes)
-        erro, erros = checa_erros(
-            datainicio, datafim, horainicio, horafim, tabela_grafico, lista_turma)
+        self.login(user is not None, fields['datainicio'], fields['datafim'], fields['horainicio'],
+                   fields['horafim'], fields['tabela_grafico'], fields['turma'], fields['questoes'])
 
-        self.response.out.write(self.navegacao())
+        erro, erros = checa_erros(fields['datainicio'], fields['datafim'], fields['horainicio'],
+                                  fields['horafim'], fields['tabela_grafico'], fields['turma'])
+
         if erro:
             self.response.out.write(self.erros(erros))
         else:
-            console = Console(is_adm, email,
-                              datainicio, datafim, horainicio, horafim, lista_turma, lista_questoes)
+            datainicial, datafinal, lista_questoes = trata_parametros(fields['datainicio'], fields['horainicio'],
+                                                                      fields['datafim'], fields['horafim'],
+                                                                      fields['questoes'])
+
+            console = Console(datainicial, datafinal, fields['turma'], lista_questoes)
+
             if not console.alunos_set:  # FIXME check this later.. what to do in an access error
                 self.response.out.write(self.erro_acesso())
-            self.cabecalho(console)
-            if tabela_grafico == "graph":
-                self.grafico(console)
+            self.cabecalho(datainicial, datafinal)
+            if fields['tabela_grafico'] == "graph":
+                self.grafico(console, datainicial, datafinal)
             else:
-                self.ajax_replace(console, tabela_grafico)
+                self.ajax_replace(fields, console)
 
-        self.response.out.write(self.formularios(
-            datainicio, datafim, horainicio, horafim, tabela_grafico, turma, questoes))
-
-    def trata_parametros(self, turma, questoes):
-
-        # FIXME This validation was invalid... turmas was always an empty set.
-        #if turma:
-        #    if turma in turmas:
-        #        turma = turma.replace(" ", "").split(",")
-        #    else:
-        #        turma = turma.split(" ")
-        #else:
-        #    turma = []
-
-        if questoes:
-            questoes = questoes.replace(" ", "").split(",")
-        else:
-            questoes = []
-
-        return turma, questoes
+        self.response.out.write(self.formularios(fields))
 
     def erros(self, erros):
-
-        erro_values = {
-            "erros": erros
-        }
-
+        erro_values = {"erros": erros}
         erro = jinja_environment.get_template("templates/erro.html")
-
         return erro.render(erro_values)
 
     def erro_acesso(self):
-
-        erro_acesso = jinja_environment.get_template(
-            "templates/erro_acesso.html")
-
+        erro_acesso = jinja_environment.get_template("templates/erro_acesso.html")
         return erro_acesso.render()
 
-    def navegacao(self):
-
-        navigation = jinja_environment.get_template(
-            "/static/templates/navigation.html")
-
-        return navigation.render()
-
-    def formularios(self, datainicio, datafim, horainicio, horafim, tabela_grafico, turma, questoes):
-
+    def formularios(self, fields):
         checkg = "false"
         checkt = "true"
-        if tabela_grafico == "graph":
+        if fields['tabela_grafico'] == "graph":
             checkg = "true"
             checkt = "false"
 
-        formularios_values = {
-            "datainicio": datainicio,
-            "datafim": datafim,
-            "horainicio": horainicio,
-            "horafim": horafim,
-            "checkg": checkg,
-            "checkt": checkt,
-            "turma": turma,
-            "questoes": questoes
-        }
+        formularios_values = dict(fields)
+
+        formularios_values["checkg"] = checkg
+        formularios_values["checkt"] = checkt
 
         formularios = jinja_environment.get_template(
             "templates/formularios.html")
@@ -226,7 +231,6 @@ class WebApp(webapp2.RequestHandler):
         return formularios.render(formularios_values)
 
     def login(self, loged, datainicio, datafim, horainicio, horafim, tabela_grafico, turma, questoes):
-
         url = str(("/?trm=%s&di=%s&hi=%s&df=%s&hf=%s&tg=%s&qts=%s") %
                   (turma, datainicio, horainicio, datafim, horafim, tabela_grafico, questoes))
 
@@ -242,68 +246,58 @@ class WebApp(webapp2.RequestHandler):
             "loger": loger
         }
         login = jinja_environment.get_template("templates/login.html")
+        self.response.out.write(login.render(login_values))
 
-        return login.render(login_values)
+        navigation = jinja_environment.get_template('static/templates/navigation.html')
+        self.response.out.write(navigation.render())
 
 
 class RefreshData(WebApp, webapp2.RequestHandler):
 
-    def tabela_submissoes(self, console):
+    def tabela_submissoes(self, console, fieds, datainicial, datafinal, email, admin):
         lista_submissoes_values = {
             "alunos": console.turma_analisada,
             "alunos_sessao": console.alunos_set,
-            "data_report": datetime.strftime(console.datainicio, "%d%m%Y"),
-            "dataf_report": datetime.strftime(console.datafim, "%d%m%Y"),
-            "email": console.email,
-            "admin": console.admin
+            "data_report": datetime.strftime(datainicial, "%d%m%Y"),
+            "dataf_report": datetime.strftime(datafinal, "%d%m%Y"),
+            "email": email,
+            "admin": admin
         }
         lista_submissoes = jinja_environment.get_template(
             "templates/lista_submissoes.html")
 
         self.response.out.write(lista_submissoes.render(lista_submissoes_values))
 
-    def tabela(self, console):
-        tabela_values = {
+    def tabela(self, console, fields, datainicial, datafinal, email, admin):
+        tabela_values = dict(fields)
+        tabela_values.update({
             "lista_questoes": sorted(console.lista_questoes),
             "alunos": console.turma_analisada,
             "alunos_sessao": console.alunos_set,
-            "datainicio": datetime.strftime(console.datainicio, "%d%m%Y"),
-            "horainicio": console.horainicio,
-            "datafim": datetime.strftime(console.datafim, "%d%m%Y"),
-            "horafim": console.horafim,
             "tabela_grafico": "graph",
-            "email": console.email,
-            "admin": console.admin
-        }
+            "email": email,
+            "admin": admin
+        })
         tabela_env = jinja_environment.get_template("templates/tabela.html")
         self.response.out.write(tabela_env.render(tabela_values))
 
     def get(self):
 
         user, is_adm, email = model.info(users.get_current_user())
-
         self.response.headers["Content-Type"] = "text/html; charset=utf-8"
-        datainicio = self.request.get("di", default_value="")
-        datafim = self.request.get("df", default_value="")
-        horainicio = self.request.get("hi", default_value="")
-        horafim = self.request.get("hf", default_value="")
-        turma = self.request.get("trm", default_value="")
-        tabela_grafico = self.request.get("tg", default_value="")
+        fields = Fields(self.request)
 
-        questoes = self.request.get("qts", default_value="")
-        if questoes:
-            questoes = questoes.replace(" ", "").split(",")
-        else:
-            questoes = []
+        datainicial, datafinal, lista_questoes = trata_parametros(fields['datainicio'], fields['horainicio'],
+                                                                  fields['datafim'], fields['horafim'],
+                                                                  fields['questoes'])
 
-        console = Console(is_adm, email,
-                          datainicio, datafim, horainicio, horafim, turma, questoes)
+        console = Console(datainicial, datafinal, fields['turma'], lista_questoes)
 
         if console.lista_questoes:
-            if tabela_grafico == "table":
-                self.response.out.write(self.tabela(console))
+            if fields['tabela_grafico'] == "table":
+                self.response.out.write(self.tabela(console, fields, datainicial, datafinal, email, is_adm))
         else:
-            self.tabela_submissoes(console)
+            self.tabela_submissoes(console, fields, datainicial, datafinal, email, is_adm)
 
 
 class Codigo(webapp2.RequestHandler):
@@ -314,8 +308,8 @@ class Codigo(webapp2.RequestHandler):
 
         user, is_adm, email = model.info(users.get_current_user())
 
-        questao = self.request.get("questao", default_value="")
-        submissao = self.request.get("submissao", default_value="")
+        #questao = self.request.get("questao", default_value="")
+        #submissao = self.request.get("submissao", default_value="")
         matricula = self.request.get("matricula", default_value="")
 
         # Service is down and we are missing k field password (it is a secret)
--- logica_console.py
+++ logica_console.py
@@ -1,79 +1,18 @@
 #  -*- coding: utf-8 -*-
-from datetime import datetime, timedelta
 import model
 
 
 class Console():
 
-    def __init__(self, admin, email, datainicio, datafim, horainicio, horafim, lista_matriculas, lista_questoes):
-
-        self.admin = admin
-        self.email = email
-        self.datainicio = datainicio
-        self.datafim = datafim
-        self.horainicio = horainicio
-        self.horafim = horafim
-
-        self._trata_parametros()
-
-        self.questoes = lista_questoes
+    def __init__(self, datainicial, datafinal, lista_matriculas, lista_questoes):
         self.lista_questoes = lista_questoes
         self.lista_matriculas = lista_matriculas
+
         # FIXME Alunos_set was already removing submissions that where not
         # from data inicio and data fim.. this must be done here now.
-        alunos, _, self.turmas = model.create_students_questions_and_classes(self.datainicio, self.datafim)
+        alunos, _, self.turmas = model.create_students_questions_and_classes(datainicial, datafinal)
         self.alunos_set = alunos.values()
         self.turma_analisada = model.search_name(alunos.values(), self.turmas, lista_matriculas)
-
-    def _trata_parametros(self):
-
-        hoje = datetime.now() - timedelta(0, 3 * 3600)
-        if not self.horainicio:
-            if 8 <= hoje.hour <= 17:
-                horainicial = timedelta(
-                    0, (hoje.hour - (hoje.hour % 2)) * 3600)
-            else:
-                horainicial = timedelta(0, 0)
-        else:
-            hora = "%02d" % int(self.horainicio[0:2])
-            minutos = "%02d" % int(self.horainicio[2:4])
-            horainicial = timedelta(
-                0, (int(hora) * 3600) + (int(minutos) * 60))
-
-        """ Tratamento Parametro Hora Fim """
-
-        if not self.horafim:
-            horafinal = timedelta(
-                0, (hoje.hour * 3600) + (hoje.minute * 60) + hoje.second)
-        else:
-            hora = self.horafim[0:2]
-            minutos = self.horafim[2:4]
-            horafinal = timedelta(0, (int(hora) * 3600) + (int(minutos) * 60))
-
-        """ Tratamento Parametro Data Fim """
-
-        if not self.datafim:
-            datafinal = datetime(hoje.year, hoje.month, hoje.day) + horafinal
-        else:
-            datafinal = datetime.strptime(self.datafim, "%d%m%Y") + horafinal
-
-        """ Tratamento Parametro Data Inicio """
-
-        if not self.datainicio:
-            datainicial = datetime(
-                hoje.year, hoje.month, hoje.day) + horainicial
-        else:
-            if self.horafim and not self.datafim:
-                datainicial = datetime.strptime(
-                    self.datainicio, "%d%m%Y") + horainicial
-                datafinal = datetime.strptime(
-                    self.datainicio, "%d%m%Y") + horafinal
-            else:
-                datainicial = datetime.strptime(
-                    self.datainicio, "%d%m%Y") + horainicial
-
-        self.datainicio = datainicial
-        self.datafim = datafinal
 
     def sumario_grafico(self):
         # creating graphical summary of one question only
